661431f0e74506f389427eea12a71e1169d855db
Stanislav Sumrin <v.nirmus@gmail.com>
2022-09-01 22:52:02 +0600

    Initial commit

---
+++ common.h
@@ -0,0 +1,12 @@
+//
+// Created by automaton62 on 9/1/22.
+//
+
+#ifndef CLOX_COMMON_H
+#define CLOX_COMMON_H
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#endif //CLOX_COMMON_H
