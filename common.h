6db3d6d1c5ff444007274f836d0311680e0a8a3b
Stanislav Sumrin <v.nirmus@gmail.com>
2022-09-02 17:09:55 +0600

    Chapter 15

--- common.h
+++ common.h
@@ -1,12 +1,10 @@
-//
-// Created by automaton62 on 9/1/22.
-//
-
 #ifndef CLOX_COMMON_H
 #define CLOX_COMMON_H
 
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
+
+#define DEBUG_TRACE_EXECUTION
 
 #endif //CLOX_COMMON_H
